5116039020fae3225dd1362922d34df377d41e37
unknown <alanambrosedev@gmail.com>
2023-08-01 23:38:26 +0530

    products seeders add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(AdminTableSeeder::class);
+        // $this->call(AdminTableSeeder::class);
         // $this->call(CmsPageTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
 
     }
 }

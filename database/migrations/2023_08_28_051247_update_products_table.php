0f9d00b6c649f25234df78ad89be09288e7c2b97
unknown <alanambrosedev@gmail.com>
2023-08-28 10:47:12 +0530

    add seeder products

---
+++ database/migrations/2023_08_28_051247_update_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('product_weight');
+        });
+
+        // Move the column to the desired position using raw SQL
+        DB::statement('ALTER TABLE products MODIFY COLUMN product_weight VARCHAR(255) AFTER product_price');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('product_weight');
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         // $this->call(AdminTableSeeder::class);
         // $this->call(CmsPageTableSeeder::class);
-        $this->call(CategoryTableSeeder::class);
+        // $this->call(CategoryTableSeeder::class);
+        $this->call(ProductTableSeeder::class);
 
     }
 }
